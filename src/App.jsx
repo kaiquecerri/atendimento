f8e5730dd0d07cad2adc8754645078c2ccd436a9
Kaique Cerri <144399326+kaiquecerri@users.noreply.github.com>
2026-04-08 20:36:10 +0000

    painel de ultimas senhas funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
 import { useNavigate } from "react-router-dom";
 
-
 function App() {
-  const navigate = useNavigate(); 
-  var i = 0;
+  const navigate = useNavigate();
+  var senha = localStorage.getItem("senhaAtual");
 
-  
-const chamarProximaSenha = (novaSenha) => {
-  localStorage.setItem('senhaAtual', novaSenha);
-}; 
+  const chamarProximaSenha = (senhaAtual) => {
+    let listaSenhas = localStorage.getItem("listaSenhas")?.split(",") || "";
+
+    if (listaSenhas == "") listaSenhas = [senhaAtual];
+    else {
+      listaSenhas.unshift(senhaAtual);
+      if (listaSenhas.length > 5) {
+        listaSenhas.pop();
+      }
+    }
+    senhaAtual++;
+    localStorage.setItem("senhaAtual", senhaAtual);
+    localStorage.setItem("listaSenhas", listaSenhas.toString());
+    senha = senhaAtual;
+  };
 
   var border = "border border-solid border-2 rounded-md p-5 border-slate-600 ";
-  var box = `flex flex-col ${border} w-100 h-100`
-  
+  var box = `flex flex-col ${border} w-100 h-100`;
+
   return (
     <div className="w-screen h-screen bg-blue-100 flex flex-row justify-center gap-5 p-5">
-      <button className={border} onClick={() => navigate("/chamadas")}> Chamadas </button>
-      <button onClick={() => {
-        chamarProximaSenha(i)
-        i++;
-        }}>CHAMAR SENHA</button>
+      <button className={border} onClick={() => navigate("/chamadas")}>
+        {" "}
+        Chamadas{" "}
+      </button>
+      <button
+        onClick={() => {
+          chamarProximaSenha(senha);
+        }}
+      >
+        CHAMAR SENHA
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Chamadas.jsx
+++ src/Chamadas.jsx
@@ -1,7 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function Chamadas() {
-  const [senha, setSenha] = useState(localStorage.getItem("senhaAtual") || "");
+  const [senha, setSenha] = useState(
+    localStorage.getItem("senhaAtual") || "---",
+  );
+
+  const [listaSenhas, setListaSenhas] = useState(
+    localStorage.getItem("listaSenhas")?.split(",") || "",
+  );
 
   useEffect(() => {
     const escutarNovaSenha = (event) => {
@@ -9,18 +15,23 @@ function Chamadas() {
         setSenha(event.newValue);
         //new Audio('/alerta.mp3').play();
       }
-    }
 
-    window.addEventListener('storage', escutarNovaSenha);
+      if (event.key == "listaSenhas") {
+        setListaSenhas(event.newValue?.split(","));
+        //new Audio('/alerta.mp3').play();
+      }
+    };
 
-    return () => window.removeEventListener('storage', escutarNovaSenha);
-  },[])
+    window.addEventListener("storage", escutarNovaSenha);
+
+    return () => window.removeEventListener("storage", escutarNovaSenha);
+  }, []);
 
   var border = "border border-solid border-2 rounded-md p-5 border-slate-600 ";
-  var box = `flex flex-col ${border} w-100 h-100`
+  var box = `flex flex-col ${border} w-100 h-100`;
   return (
     <div className="w-screen h-screen bg-blue-100 flex flex-row justify-center gap-5 p-5">
-      <div className={box} >
+      <div className={box}>
         <h1 className="font-bold text-3xl">Próximas senhas</h1>
         <ul className="ml-3 mt-3 text-xl">
           <li>100</li>
@@ -31,14 +42,17 @@ function Chamadas() {
       <div className={box}>
         <h1 className="font-bold text-3xl">Senhas chamadas</h1>
         <ul className="ml-3 mt-3 text-xl">
-          <li><h1 className="font-bold text-2xl text-red-700">{senha}</h1> </li>
-          <li>98</li>
-          <li>97</li>
+          <li>
+            <h1 className="font-bold text-2xl text-red-700">{senha}</h1>{" "}
+          </li>
+          <ul>
+            {listaSenhas.length > 0 &&
+              listaSenhas.map((s, index) => <li key={index}>{s}</li>)}
+          </ul>
         </ul>
       </div>
-      
     </div>
-    );
+  );
 }
 
 export default Chamadas;
