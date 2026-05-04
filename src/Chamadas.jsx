b05be0a46ebacb3105c8f6e3af76221e7c9be94b
Kaique <kaiquecerrifialho1@gmail.com>
2026-05-04 13:47:35 -0300

    chamadas

--- src/Chamadas.jsx
+++ src/Chamadas.jsx
@@ -130,37 +130,3 @@ function Chamadas() {
 }
 
 export default Chamadas;
-/*
-<div className="w-screen h-screen bg-blue-100 flex flex-row justify-center gap-5 p-5">
-      <div className={box}>
-        <h1 className="font-bold text-3xl">Próximas senhas</h1>
-        <div className="flex flex-row">
-          <ul className="ml-3 mt-3 text-xl w-[50%]">
-            <h1>Comum</h1>
-            {senhasCom.length > 0 &&
-              senhasCom.slice(0, 8).map((s, index) => <li key={index}>{s}</li>)}
-              {senhasCom.length > 8 && <li>...</li>}
-          </ul>
-
-          <ul className="ml-3 mt-3 text-xl w-[50%]">
-            <h1>Preferencial</h1>
-            {senhasPref.length > 0 &&
-              senhasPref.slice(0, 8).map((s, index) => <li key={index}>{s}</li>)}
-              {senhasPref.length > 8 && <li>...</li>}
-          </ul>
-        </div>
-      </div>
-      <div className={box}>
-        <h1 className="font-bold text-3xl">Senhas chamadas</h1>
-        <ul className="ml-3 mt-3 text-xl">
-          <li>
-            <h1 className="font-bold text-2xl text-red-700">{listaSenhas[0]}</h1>{" "}
-          </li>
-          <ul>
-            {listaSenhas.length > 0 &&
-              listaSenhas.slice(1, 6).map((s, index) => <li key={index}>{s}</li>)}
-          </ul>
-        </ul>
-      </div>
-    </div>
-*/
